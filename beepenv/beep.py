76bcf99f40b5a834b2c1c0ace18882d9b4337e40
Wahyu Haryanto <way3haryanto@gmail.com>
2023-04-08 13:55:01 +0700

    using 6v to get higher beep

--- beepenv/beep.py
+++ beepenv/beep.py
@@ -1,35 +1,18 @@
 import RPi.GPIO as GPIO
 import time
 
-# Set the GPIO pin number
-BEEP_PIN = 18
-
-# Set up the GPIO pin as an output
+# Set up GPIO
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(BEEP_PIN, GPIO.OUT)
+GPIO.setup(17, GPIO.OUT)
 
-# Define the beep function
-def beep(frequency, duration):
-    # Calculate the period of the tone in microseconds
-    period = 1000000 // frequency
-    # Calculate the duration of each half-period in microseconds
-    half_period = period // 2
-    # Calculate the number of cycles to generate for the specified duration
-    cycles = int(duration * frequency)
-    # Generate the tone by toggling the beep pin on and off
-    for i in range(cycles):
-        GPIO.output(BEEP_PIN, GPIO.HIGH)
-        time.sleep(half_period / 1000000.0)
-        GPIO.output(BEEP_PIN, GPIO.LOW)
-        time.sleep(half_period / 1000000.0)
+# Turn on the 6-volt pin to generate a beep
+GPIO.output(17, GPIO.HIGH)
 
-# Define the notes for the simple song
-notes = [262, 294, 330, 349, 392, 440, 494, 523]
+# Wait for half a second
+time.sleep(0.5)
 
-# Play the simple song
-for note in notes:
-    beep(note, 0.5)
-    time.sleep(0.1)
+# Turn off the 6-volt pin to stop the beep
+GPIO.output(17, GPIO.LOW)
 
-# Clean up the GPIO pin
+# Clean up GPIO
 GPIO.cleanup()
