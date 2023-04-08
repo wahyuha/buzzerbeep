fb62dc77ff114464568955ab4a98d94c5f52f9b6
Wahyu Haryanto <way3haryanto@gmail.com>
2023-04-08 14:46:36 +0700

    update ambulance

--- beepenv/ambulance.py
+++ beepenv/ambulance.py
@@ -6,17 +6,15 @@ GPIO.setmode(GPIO.BCM)
 buzzer_pin = 21
 GPIO.setup(buzzer_pin, GPIO.OUT)
 
-# Beep every half second for 5 seconds
-for i in range(5):
-    if i == 4:
-        # Last beep is 2 second
-        duration = 2
-    else:
-        duration = 0.5
+# Define the beep pattern
+beep_pattern = [0.05, 0.05, 0.05, 0.1, 0.1, 0.1, 0.2, 0.2, 0.2, 0.2, 0.1, 0.1, 0.1, 0.05, 0.05, 0.05]
+
+# Beep the buzzer pin in the defined pattern
+for duration in beep_pattern:
     GPIO.output(buzzer_pin, GPIO.HIGH)
     time.sleep(duration)
     GPIO.output(buzzer_pin, GPIO.LOW)
-    time.sleep(0.5)
+    time.sleep(duration)
 
 # Clean up the GPIO pins
 GPIO.cleanup()
