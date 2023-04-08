ceec4417845064b51a9f1ab07fa75c769530438d
Wahyu Haryanto <way3haryanto@gmail.com>
2023-04-08 14:31:09 +0700

    beep every second for 5 second

--- beepenv/beep.py
+++ beepenv/beep.py
@@ -1,34 +1,22 @@
 import RPi.GPIO as GPIO
 import time
 
-# Set up GPIO
+# Set up the GPIO pins
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(21, GPIO.OUT)
+buzzer_pin = 21
+GPIO.setup(buzzer_pin, GPIO.OUT)
 
-# Define the length of each beep in seconds
-beep_length = 0.5
+# Beep every half second for 5 seconds
+for i in range(10):
+    if i == 9:
+        # Last beep is 1 second
+        duration = 1
+    else:
+        duration = 0.5
+    GPIO.output(buzzer_pin, GPIO.HIGH)
+    time.sleep(duration)
+    GPIO.output(buzzer_pin, GPIO.LOW)
+    time.sleep(0.1)
 
-# Define the number of beeps to generate
-num_beeps = int(5 / beep_length)
-
-# Generate the beeps
-for i in range(num_beeps):
-    # Turn on the 6-volt pin to generate a beep
-    GPIO.output(21, GPIO.HIGH)
-    
-    # Wait for the length of the beep
-    time.sleep(beep_length)
-    
-    # Turn off the 6-volt pin to stop the beep
-    GPIO.output(21, GPIO.LOW)
-    
-    # Wait for a short pause between beeps
-    time.sleep(beep_length / 2)
-
-# Generate a longer last beep
-GPIO.output(21, GPIO.HIGH)
-time.sleep(beep_length * 2)
-GPIO.output(21, GPIO.LOW)
-
-# Clean up GPIO
+# Clean up the GPIO pins
 GPIO.cleanup()
