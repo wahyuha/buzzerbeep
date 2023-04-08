7e38f99e4171acb31d2fb8d99889943e31533af1
Wahyu Haryanto <way3haryanto@gmail.com>
2023-04-08 14:12:17 +0700

    updat with simple song

--- beepenv/beep.py
+++ beepenv/beep.py
@@ -5,30 +5,80 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
 
-# Define the length of each beep in seconds
-beep_length = 0.1
+# Define the frequency of each note in Hz
+note_freqs = {
+    'C': 261.63,
+    'D': 293.66,
+    'E': 329.63,
+    'F': 349.23,
+    'G': 392.00,
+    'A': 440.00,
+    'B': 493.88
+}
 
-# Define the number of beeps to generate
-num_beeps = int(5 / beep_length)
+# Define the length of each note in seconds
+note_lengths = {
+    'whole': 1.0,
+    'half': 0.5,
+    'quarter': 0.25,
+    'eighth': 0.125,
+    'sixteenth': 0.0625
+}
 
-# Generate the beeps
-for i in range(num_beeps):
-    # Turn on the 6-volt pin to generate a beep
-    GPIO.output(17, GPIO.HIGH)
-    
-    # Wait for the length of the beep
-    time.sleep(beep_length)
-    
-    # Turn off the 6-volt pin to stop the beep
-    GPIO.output(17, GPIO.LOW)
-    
-    # Wait for a short pause between beeps
-    time.sleep(beep_length / 2)
+# Define the notes of the song
+song_notes = [
+    ('E', 'quarter'),
+    ('E', 'quarter'),
+    ('F', 'quarter'),
+    ('G', 'half'),
+    ('G', 'quarter'),
+    ('F', 'quarter'),
+    ('E', 'quarter'),
+    ('D', 'half'),
+    ('C', 'quarter'),
+    ('C', 'quarter'),
+    ('D', 'quarter'),
+    ('E', 'half'),
+    ('E', 'quarter'),
+    ('D', 'quarter'),
+    ('D', 'half'),
+    ('E', 'quarter'),
+    ('E', 'quarter'),
+    ('F', 'quarter'),
+    ('G', 'half'),
+    ('G', 'quarter'),
+    ('F', 'quarter'),
+    ('E', 'quarter'),
+    ('D', 'half'),
+    ('C', 'quarter'),
+    ('C', 'quarter'),
+    ('D', 'quarter'),
+    ('E', 'half'),
+    ('D', 'quarter'),
+    ('C', 'quarter'),
+    ('C', 'half')
+]
 
-# Generate a longer last beep
-GPIO.output(17, GPIO.HIGH)
-time.sleep(beep_length * 2)
-GPIO.output(17, GPIO.LOW)
+# Play the song
+for note, length in song_notes:
+    # Calculate the duration of the note in seconds
+    duration = note_lengths[length]
+    
+    # Calculate the frequency of the note in Hz
+    frequency = note_freqs[note]
+    
+    # Generate the tone for the note
+    tone = GPIO.PWM(17, frequency)
+    tone.start(50)
+    
+    # Wait for the duration of the note
+    time.sleep(duration)
+    
+    # Stop the tone
+    tone.stop()
+    
+    # Wait for a short pause between notes
+    time.sleep(duration / 4)
 
 # Clean up GPIO
 GPIO.cleanup()
